de11d1b4a99139541388a71ad40593b60f2f1944
Tom Sterritt <tom@tomsterritt.com>
2013-06-15 12:38:18 +0100

    Initial commit

--- README.md
+++ README.md
@@ -2,3 +2,32 @@ Charts.Aero-phpVMS
 ==================
 
 phpVMS class for retrieving charts from Charts.aero
+
+Usage
+-----
+Ensure you enter your mashape authorisation key in the static variable at the top of the class.
+
+To retrieve by ICAO:
+`` $data = AeroCharts::GetByICAO('KRDM'); ``
+
+To retrieve by IATA:
+`` $data = AeroCharts::GetByIATA('RDM'); ``
+
+Both of the above will either return false (handle yourself), or an object, which can be used as follows (for example):
+
+`` $airportName = $data->airportName;	// Redmond Roberts Field
+$city = $data->city;					// Redmond
+$icao = $data->icao;					// KRDM
+$iata = $data->iata;					// RDM
+foreach($data->charts as $chart){
+	 $chartName 	= $chart->chart_name;		// "TAKEOFF MINIMUMS"
+	 $accurateOf 	= $chart->accurate_as_of;	// "1358507045"
+	 $fileURL 		= $chart->file_url;			// "http://a1d40817df887201f900-46f76acc0dd0843121c39db1cec939a0.r80.cf1.rackcdn.com/1-NW1TO.pdf"
+	 $fileSize 		= $chart->file_size;		// "226725"
+	 $fileThumbnail	= $chart->thumbnail_url;	// "http://a1d40817df887201f900-46f76acc0dd0843121c39db1cec939a0.r80.cf1.rackcdn.com/1-NW1TO.png"
+}``
+
+Also includes the Search function, but **does not** format response. See charts.aero documentation for response format.
+`` $data = AeroCharts::Search('Portland'); ``
+
+**NOTICE:** This has not and will not be tested, so I make no guarantee that this will work, however will maintain this repo, so feel free to create issues or pull requests for changes. 
---
+++ core/common/AeroCharts.class.php
@@ -0,0 +1,77 @@
+<?php
+/*
+ * Charts.aero for phpVMS
+ *
+ * Copyright 2013, Tom Sterritt - http://sterri.tt
+ * Released under the Creative Commons Attribution-NonCommercial-ShareAlike Licence
+ * http://creativecommons.org/licenses/by-nc-sa/3.0/
+ *
+ * Github:  https://github.com/tomsterritt/Charts.Aero-phpVMS
+ * Version: 1.0.0
+ */
+
+class AeroCharts extends CodonData {
+	
+	// Enter your mashape.com authorisation key here.
+	private static $maShapeKey = "";
+	
+	public static function Search($query){
+		// This doesn't return a particularly useful response, but handle it if you wish
+		$data = array('query' => $query);
+		return self::MakeRequest('https://charts.p.mashape.com/search', $data);
+	}
+	
+	public static function GetByICAO($icao){
+		// Basic valiation
+		if(strlen($icao) > 4 || strlen($icao) == 0){
+			return false;
+		}
+		$data = array('icao' => $icao);
+		return self::ChartArray(self::MakeRequest('https://charts.p.mashape.com/retrieve/icao', $data));
+	}
+	
+	public static function GetByIATA($iata){
+		// Basic validation
+		if(strlen($iata) > 3 || strlen($iata) == 0){
+			return false;
+		}
+		$data = array('iata' => $icao);
+		return self::ChartArray(self::MakeRequest('https://charts.p.mashape.com/retrieve/iata', $data));
+	}
+	
+	private static function MakeRequest($url, $data){
+		$curl = curl_init();
+		
+		curl_setopt_array($curl, array(
+			CURLOPT_RETURNTRANSFER => 1,
+			CURLOPT_URL => $url,
+			CURLOPT_POST => 1,
+			CURLOPT_POSTFIELDS => $data,
+			CURLOPT_HTTPHEADER => array(
+				'X-Mashape-Authorization' => self::$maShapeKey
+			)
+		));
+		
+		return curl_exec($curl);
+		curl_close($curl);
+	}
+	
+	private static function ChartArray($data){
+		$response = json_decode($data);
+		if($response == NULL || $response['status'] !== 'success'){
+			// Either it's broken or you don't have a subscription. Display your own friendly message.
+			return false;
+		}
+		
+		$ret = array(
+			'airportName' 	=> $response['airport']['airport_name'],
+			'city' 			=> $response['airport']['city'],
+			'icao' 			=> $response['airport']['icao'],
+			'iata' 			=> $response['airport']['iata'],
+			'charts' 		=> (object)$response['data']
+		);
+		return (object)$ret;
+	}
+}
+
+?>
